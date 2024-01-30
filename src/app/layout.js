dac0a03e84bcec8045e719bd8f8e585d85c28c3d
JinHyeok <gjwlsgur123@gmail.com>
2024-01-30 23:57:45 +0900

    Fix: OG Image 변경

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import Head from "next/head";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "My Portfolio",
+  title: "Jinhyeok's Portfolio",
   description: "Frontend Developer 허진혁의 포트폴리오입니다.",
 };
 
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <title>My page title</title>
+        <title>Jinhyeok's Portfolio</title>
         <meta property="og:title" content="Jinhyeok's Portfolio" />
         <meta
           property="og:description"
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
         />
         <meta
           property="og:image"
-          content="https://ia.media-imdb.com/images/about-image.jpeg"
+          content="https://jinhyeok-portfolio.netlify.app/images/about-image.jpeg"
         />
       </Head>
       <body className={inter.className}>{children}</body>
