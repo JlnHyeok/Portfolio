4a5df75ab2ffbbf2608cb785babcc41f9ef7cdfa
JinHyeok <gjwlsgur123@gmail.com>
2024-01-31 23:43:10 +0900

    Fix: og image 수정

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Jinhyeok Portfolio",
   description: "Frontend Developer 허진혁의 포트폴리오입니다.",
+  metadataBase: new URL("https://jinhyeok-portfolio.netlify.app/"),
 };
 
 export default function RootLayout({ children }) {
