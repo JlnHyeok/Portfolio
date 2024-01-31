a102333a57819ac670a11933c6a8cc06e25caa14
JinHyeok <gjwlsgur123@gmail.com>
2024-01-31 23:25:15 +0900

    Fix: js-> jsx 수정

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,27 +5,10 @@ import ProjectsSection from "./components/ProjectsSection";
 import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
 import AchievementsSection from "./components/AchievementsSection";
-import Head from "next/head";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212] ">
-      <Head>
-        <title>Jinhyeok Portfolio</title>
-        <meta property="og:title" content="Jinhyeok Portfolio" />
-        <meta
-          property="og:description"
-          content="Frontend Developer 허진혁의 포트폴리오입니다."
-        />
-        <meta
-          property="og:url"
-          content="https://jinhyeok-portfolio.netlify.app/"
-        />
-        <meta
-          property="og:image"
-          content="https://jinhyeok-portfolio.netlify.app/images/about-image.jpeg"
-        />
-      </Head>
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
